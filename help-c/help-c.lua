e130085e90bbf04ae76f40a21ca0443fcf9ce24e
AdamsDevelopment <132950018+AdamsDevelopment@users.noreply.github.com>
2023-05-08 19:24:18 -0500

    Delete help-c.lua

--- help-c/help-c.lua
+++
@@ -1,7 +0,0 @@
-RegisterCommand("help", function()
-    msg("Join our discord for help: [Replace Me: Place Your discord link here] ")
-end, false)
-
-function msg(text)
-    TriggerEvent("chatMessage", "[Server]", {255,0,0}, text)
-end    
